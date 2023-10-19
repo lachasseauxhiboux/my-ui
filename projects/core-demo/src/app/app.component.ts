6e24d88e4bdc83fd194084e7771588daa2c79463
Pavel Kuldyshev <pk@fabuza.ru>
2023-10-19 14:57:09 +0300

    Was added button module, grid and etc. refactored layout, mixins.

--- projects/core-demo/src/app/app.component.ts
+++ projects/core-demo/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Section, sections } from './sections';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Theme, ThemeService } from '@core';
@@ -6,10 +6,13 @@ import { Theme, ThemeService } from '@core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   readonly SECTIONS = sections;
+
+  readonly showFooter = true;
 
   activeSection = sections[0];
 
